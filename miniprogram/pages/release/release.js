e1a2d80e869a4e0930a0dde94d8d894a9a405c3e
YanZhuangZhuang <yz808@outlook.com>
2021-04-30 16:39:21 +0800

    feat: 发布时图片上传

--- miniprogram/pages/release/release.js
+++ miniprogram/pages/release/release.js
@@ -1,25 +1,75 @@
+const {$Message} = require('../../components/base/index');
+
 Page({
     data: {
         name: '',
         price: '',
         stock: 1,
-        state: true
+        state: true,
+        files: []
     },
     onLoad: function (options) {
-
+        this.setData({
+            selectFile: this.selectFile.bind(this),
+            uplaodFile: this.uplaodFile.bind(this)
+        })
     },
     handleChangeStock({detail}) {
         this.setData({
             stock: detail.value
         })
     },
-    onChange(event) {
+    handleChangeState(event) {
         const detail = event.detail;
         this.setData({
             state: detail.value
         })
     },
-    handleClick() {
+    handleSubmit() {
         console.log(this.data.state)
+    },
+    chooseImage: function (e) {
+        var that = this;
+        wx.chooseImage({
+            // sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+            // sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+            success: function (res) {
+                // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+                that.setData({
+                    files: that.data.files.concat(res.tempFilePaths)
+                });
+            }
+        })
+    },
+    previewImage: function (e) {
+        wx.previewImage({
+            current: e.currentTarget.id, // 当前显示图片的http链接
+            urls: this.data.files // 需要预览的图片http链接列表
+        })
+    },
+    selectFile(files) {
+        // 选择图片时的过滤函数，返回true表示图片有效
+        console.log('files', files)
+    },
+    uplaodFile(files) {
+        console.log('upload files', files)
+        // 文件上传的函数，返回一个promise
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                reject('some error')
+            }, 1000)
+        })
+    },
+    uploadError(e) {
+        $Message({
+            content: '上传失败',
+            type: 'warning'
+        });
+    },
+    uploadSuccess(e) {
+        $Message({
+            content: '上传成功',
+            type: 'success'
+        });
     }
 });
