2aebb7b166d012f035c6076e0491ddf2e9e9a57a
jonssonyan <yz808@outlook.com>
2021-04-30 22:13:41 +0800

    feat: 登录

--- miniprogram/pages/me/me.js
+++ miniprogram/pages/me/me.js
@@ -1,36 +1,32 @@
 Page({
     data: {
-        avatarUrl: '',
-        nickName: '',
-        result: false,
-        canIUse: wx.canIUse('button.open-type.getUserInfo')
+        userInfo: {},
+        hasUserInfo: false,
+        canIUseGetUserProfile: false
     },
     onLoad: function (options) {
-
+        if (wx.getUserProfile) {
+            this.setData({
+                canIUseGetUserProfile: true
+            })
+        }
     },
     // 请求API授权，获得用户头像和昵称
-    bindGetUserInfo(e) {
-        // 查看是否授权
-        wx.getSetting({
-            success(res) {
-                if (res.authSetting['scope.userInfo']) {
-                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-                    wx.getUserProfile({
-                        success: function (res) {
-                            that.setData({
-                                result: true,// 结果
-                                nickName: res.userInfo.nickName,// 微信昵称
-                                avatarUrl: res.userInfo.avatarUrl,// 微信头像
-                            })
-                        }
-                    })
-                } else {
-                    // 未授权，结果返回null
-                    that.setData({
-                        result: false,// 结果
-                    })
-                }
+    getUserProfile(e) {
+        wx.getUserProfile({
+            desc: '用于完善会员资料',
+            success: (res) => {
+                this.setData({
+                    userInfo: res.userInfo,
+                    hasUserInfo: true
+                })
             }
+        })
+    },
+    getUserInfo(e) {
+        this.setData({
+            userInfo: e.detail.userInfo,
+            hasUserInfo: true
         })
     }
 });
