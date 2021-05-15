1caa7f110e09dfee96d6d8a79ace19a5a96222c5
Jonsson <yz808@outlook.com>
2021-05-16 00:46:12 +0800

    fix: 优化我发布的和我卖出的

--- cloudfunctions/selectPageOrder/index.js
+++ cloudfunctions/selectPageOrder/index.js
@@ -7,11 +7,26 @@ const db = cloud.database();
 exports.main = async (event, context) => {
     const {pageNum = 1, pageSize = 10, filter = {}} = event;
 
-    let orders = await db.collection('order').where(filter)
+    let orders = await db.collection('order')
+        .where(filter)
         .skip((pageNum - 1) * pageSize)
-        .limit(pageSize).get().then(res => res);
-    for (let order in orders) {
-        order.product = await db.collection('product').where({_id: order.product_id}).get()
+        .limit(pageSize).get();
+    let res = orders.data;
+    for (let i = 0; i < res.length; i++) {
+        res[i].product = await db.collection('product').where({_id: res[i].product_id}).limit(1).get().then(res => res.data[0])
+        res[i].createTime = dataToString(res[i].create_time)
     }
     return orders;
 };
+
+// 时间戳转换为 yyyy-MM-dd HH:mm:ss 的字符串格式
+function dataToString(time) {
+    let da = new Date(time);
+    let year = da.getFullYear();
+    let month = da.getMonth() + 1;
+    let date = da.getDate();
+    let hours = da.getHours();
+    let minutes = da.getMinutes();
+    let seconds = da.getSeconds();
+    return [year, month, date].join("-") + " " + [hours, minutes, seconds].join(':');
+}
--- miniprogram/pages/sell/sell.js
+++ miniprogram/pages/sell/sell.js
@@ -9,7 +9,7 @@ Page({
         // 订单列表
         orders: [],
         // 查询订单时的参数对象
-        param: {
+        orderParam: {
             dbName: 'order',
             pageNum: 1,
             pageSize: 10,
@@ -17,13 +17,13 @@ Page({
                 open_id: null
             }
         },
-        // 查询商品时的参数对象
-        productParam: {
-            dbName: 'product',
-            filter: {}
-        },
-        // 存放 商品id和商品名称 键值对Map
-        productMap: null
+        // // 查询商品时的参数对象
+        // productParam: {
+        //     dbName: 'product',
+        //     filter: {}
+        // },
+        // // 存放 商品id和商品名称 键值对Map
+        // productMap: null
     },
 
     /**
@@ -79,7 +79,7 @@ Page({
     onReachBottom: function () {
         let that = this;
         this.setData({
-            ['param.pageNum']: that.data.param.pageNum++
+            ['param.pageNum']: that.data.orderParam.pageNum++
         })
         this.selectPage().then(res => {
             that.data.orders.push(res)
@@ -96,56 +96,41 @@ Page({
         console.log(e)
     },
     async selectPage() {
-        const that = this;
+
         // 获取商品列表，商品id=>商品名称
-        await wx.cloud.callFunction({
-            name: 'selectList',
-            data: that.data.productParam
-        }).then((e) => {
-            let map = new Map();
-            let productList = e.result.data;
-            for (let i = 0; i < productList.length; i++) {
-                map.set(productList[i]._id, productList[i].name)
-            }
-            that.setData({
-                ['productMap']: map
-            })
-        })
+        // await wx.cloud.callFunction({
+        //     name: 'selectList',
+        //     data: that.data.productParam
+        // }).then((e) => {
+        //     let map = new Map();
+        //     let productList = e.result.data;
+        //     for (let i = 0; i < productList.length; i++) {
+        //         map.set(productList[i]._id, productList[i].name)
+        //     }
+        //     that.setData({
+        //         ['productMap']: map
+        //     })
+        // })
+        const that = this;
         let orders = [];
+        // 设置当前用户的openid
         await wx.cloud.callFunction({
             name: 'getWXContext'
         }).then(e => {
-            // 获取订单列表
             that.setData({
-                ['param.filter.seller']: e.result.openid
+                ['orderParam.filter.seller']: e.result.openid
             })
-
         })
-        if (that.data.param.filter.seller) {
+        if (that.data.orderParam.filter.seller) {
+            // 获取订单列表
             await wx.cloud.callFunction({
                 name: 'selectPageOrder',
-                data: that.data.param
+                data: that.data.orderParam
             }).then((e) => {
-                orders = e.result.data;
-                console.log(orders)
-                // 遍历订单列表，获取商品的名称，两个云函数嵌套，得到的数据没有渲染到界面上，所以采用存在data中然后在取值的方法
-                for (let i = 0; i < orders.length; i++) {
-                    orders[i].productName = that.data.productMap.get(orders[i].product_id)
-                    orders[i].create_time = this.dataToString(orders[i].create_time)
-                }
+                orders = e.result.data
             })
         }
+        console.log(orders)
         return orders;
-    },
-    // 时间戳转换为 yyyy-MM-dd HH:mm:ss 的字符串格式
-    dataToString(time) {
-        let da = new Date(time);
-        let year = da.getFullYear();
-        let month = da.getMonth() + 1;
-        let date = da.getDate();
-        let hours = da.getHours();
-        let minutes = da.getMinutes();
-        let seconds = da.getSeconds();
-        return [year, month, date].join("-") + " " + [hours, minutes, seconds].join(':');
     }
 });
